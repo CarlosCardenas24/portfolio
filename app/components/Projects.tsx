f1d3f73970aca50daad38ee5737adc2f5a9bc259
CarlosCardenas24 <nooptions101@gmail.com>
2025-04-19 15:45:09 -0700

    grid for Projects.tsx is done, still stuck on the bg

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -24,10 +24,7 @@ const Projects: React.FC<ProjectsProps> = ({ recommendation }) => {
                   className="rounded-full w-[90px] h-[90px] object-cover border-2 border-white"
                 />
               </div>
-              <div className="flex flex-col justify-center gap-1">
-                {/* <span className="text-white">Carlos Cardenas</span>
-            <span className="text-white">Software Developer</span> */}
-              </div>
+              <div className="flex flex-col justify-center gap-1"></div>
             </div>
             <div className="max-w-md">
               <p className="text-[18px] text-white max-w-md">
@@ -36,66 +33,52 @@ const Projects: React.FC<ProjectsProps> = ({ recommendation }) => {
             </div>
           </div>
 
-          <div className="w-full mt-16 pb-16 px-8 relative">
-            <div className="w-[80%] h-[450px] mx-auto my-20 relative overflow-hidden flex items-center justify-center">
-              <div className="w-[100%] h-[100%] mx-auto my-20 relative overflow-hidden flex items-center justify-center">
+          <div className="w-full mt-16 pb-16 px-8">
+            <div className="grid grid-cols-10 grid-rows-5 w-[80%] h-[450px] mx-auto relative">
+              <div className="col-start-5 col-span-2 row-start-1 flex justify-center items-center">
+                <a
+                  href="#"
+                  className="text-[25px] hover:underline whitespace-nowrap p-[20px] block"
+                >
+                  Shopify Theme App Extension
+                </a>
+              </div>
+              <div className="col-start-1 row-start-3 flex items-center justify-center -rotate-90">
+                <a
+                  href="#"
+                  className="text-[25px] hover:underline p-[20px] whitespace-nowrap block"
+                >
+                  Landing Page
+                </a>
+              </div>
+
+              <div className="col-start-2 col-span-8 row-start-2 row-span-3 relative overflow-hidden w-full">
                 <img
                   src="/leopard-bg.png"
                   alt="Leopard Background"
-                  className="absolute w-full h-full object-cover transform rotate-90"
+                  className="absolute insert-0 w-full h-full max-w-270 max-h-778 object-fill transform rotate-90"
                 />
               </div>
-              {/* <div
-            className="absolute inset-0 bg-center bg-no-repeat"
-            style={{
-              backgroundImage: "url('/leopard-bg.png')",
-              backgroundSize: "100% 100%",
-              width: "100%",
-              height: "100%",
-              transform: "rotate(90deg)",
-            }}
-          ></div> */}
 
-              <div className="absolute inset-0 flex items-center justify-center">
+              <div className="col-start-5 col-span-2 row-start-2 row-span-3 absolute inset-0 flex items-center justify-center">
                 <h3 className="text-[55px] text-white z-10">Projects</h3>
               </div>
-            </div>
-            {/* <div className="bg-[url('/leopard-bg.png')] bg-center bg-no-repeat flex items-center justify-center w-[80%] h-[450px] mx-auto my-20">
-          <h3 className="text-[55px]">Projects</h3>
-        </div> */}
-
-            <div className="absolute top-4 left-1/2 transform -translate-x-1/2 w-[400px] text-center">
-              <a
-                href="#"
-                className="text-[25px] hover:underline whitespace-nowrap p-[20px] block"
-              >
-                Shopify Theme App Extension
-              </a>
-            </div>
-
-            <div className="absolute right-4 top-1/2 transform -translate-y-1/2 w-[80px] h-[300px]">
-              <a
-                href="#"
-                className="text-[25px] hover:underline p-[20px] block"
-              >
-                Landing Page
-              </a>
-            </div>
-            <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 w-[400px] text-center">
-              <a
-                href="#"
-                className="text-[25px] hover:underline p-[20px] whitespace-nowrap block"
-              >
-                AI Image Diffusion / Story Board
-              </a>
-            </div>
-            <div className="absolute left-4 top-1/2 transform -translate-y-1/2 w-[80px] h-[300px]">
-              <a
-                href="#"
-                className="text-[25px] hover:underline p-[20px] block"
-              >
-                Shopping Cart
-              </a>
+              <div className="col-start-10 row-start-3 flex items-center justify-center rotate-90">
+                <a
+                  href="#"
+                  className="text-[25px] hover:underline p-[20px] whitespace-nowrap block"
+                >
+                  Shopping Cart
+                </a>
+              </div>
+              <div className="col-start-5 col-span-2 row-start-5 flex justify-center items-center">
+                <a
+                  href="#"
+                  className="text-[25px] hover:underline p-[20px] whitespace-nowrap block"
+                >
+                  AI Image Diffusion / Story Board
+                </a>
+              </div>
             </div>
           </div>
         </div>
